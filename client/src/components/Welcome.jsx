796ea8d2359a5fb6d63ce3c6298ba259ef30f352
Anmol <95535448+AnmolSirola@users.noreply.github.com>
2023-01-10 14:11:46 +0530

    Importing components

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,3 +1,13 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { TransactionContext } from "../context/TransactionContext";
+import { shortenAddress } from "../utils/shortenAddress";
+import { Loader } from ".";
+
+
+
 const Welcome = () => {
     return(
         <h1>Welcome</h1>
