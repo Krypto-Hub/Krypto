f2cc27f502abd521654e3da1f379ec6b093fafdc
Anmol <95535448+AnmolSirola@users.noreply.github.com>
2023-01-21 21:40:32 +0530

    Input.jsx

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -7,10 +7,7 @@ import dummyData from "../utils/dummyData";
 import { shortenAddress } from "../utils/shortenAddress";
 
 
-const Transactions = () => {
-    return(
-        <h1>Transactions</h1>
-    );
-}
+const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
+    const gifUrl = useFetch({ keyword });
 
 export default Transactions;
